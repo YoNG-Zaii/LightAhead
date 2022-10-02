39415985ed056b4509b4cb844003397b2ec22c6b
YoNG-Zaii <yongzaii811@outlook.com>
2022-10-02 22:22:08 +0800

    Changed name order

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -10,8 +10,8 @@ const HomePage = () => {
                     <div className='Team'>
                         <h3>Avengers Assemble</h3>
                         <button><a href='https://github.com/blws02'>Bernard Lim</a></button>
-                        <button><a href='https://github.com/Horstann'>Horstann Ho</a></button>
                         <button><a href='https://github.com/YoNG-Zaii/'>Guo Yong</a></button>
+                        <button><a href='https://github.com/Horstann'>Horstann Ho</a></button>
                         <button><a href='https://github.com/Pratham117'>Pratham</a></button>
                         <button><a href='https://github.com/SLAU925'>Shao Wei</a></button>
                     </div>
