1049ef57aee172d308ff7f88a32c2e0147c815ed
YoNG-Zaii <yongzaii811@outlook.com>
2022-10-02 14:30:43 +0800

    Updated Problem component

--- src/components/Problem/Problem.js
+++ src/components/Problem/Problem.js
@@ -4,10 +4,10 @@ const Problem = () => {
     return <div className="problem-box">
         <h2>Problem Statement</h2>
         <div className="problem-description">
-            <h4>Topic: AI and Smart Nation</h4>
-            <p>“Develop AI models or IoT solutions that solve industrial or social problems in the new stage of
+            <h3>Topic: AI and Smart Nation</h3>
+            <p>Develop AI models or IoT solutions that solve industrial or social problems in the new stage of
                 society development. The Smart Nation is an initiative by the Government of Singapore to harness
-                from info-comm technologies, networks, and big data to create tech-enabled solutions.”</p>
+                from info-comm technologies, networks, and big data to create tech-enabled solutions.</p>
         </div>
     </div> 
 }
