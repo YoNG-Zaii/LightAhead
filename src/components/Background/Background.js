9f4ffb8e6b9ffc3edcc1d6cb697fc4fc9cd1af67
ShaoWei <laushaowei@gmail.com>
2022-10-02 14:49:19 +0800

    content changed

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -7,8 +7,7 @@ const Background = () => {
             <div className="background-content">
                 <h2>The Blind Community</h2>
                 <div>
-                    <p>Having to deal with sight loss or low vision is merely one of the challenges that the visually impaired are facing when living life. Blind individuals are just like anyone else but they just can’t see. Is this case as simple in real life as in our minds, though? Any individual different from the average, such as the visually impaired, face difficulties because they’re not what is considered to be average. This article aims to shed light upon the challenges the visually impaired face just by living life and being the odd ones out.</p>
-                    <p>Being blind in a world suited for the sighted, means there will be multiple normal mishaps. Stumbling upon an office chair that wasn’t neatly tucked under the desk or knocking a glass off the table because it was left right on the edge, are small accidents that can happen and that’s okay. However, such mishaps tend to be perceived by sighted individuals as the inability of the visually impaired to perform tasks, while, in reality, they stem from the inaccessibility of our world. Blindness or low vision does not indicate the intelligence of the individual nor how sad their life is. Just because the sighted cannot imagine their world without vision does not mean that the visually impaired have a sad or unhappy life because of their visual condition.</p>
+                    <p>In this day and age, having to deal with sight loss is one of the notable challenges that the visually impaired are facing when living life. Blind individuals are just like anyone else but they just can’t see. Becoming aware of objects within an environment or route can make it so much easier for a blind person to navigate around successfully. Therefore, our invention aims to help solve the problems that this community would be facing in manuevering around.</p>
                 </div>
             </div>
             <img className="image" src={blindComm} alt=""/>
@@ -18,15 +17,9 @@ const Background = () => {
             <div className="background-content">
                 <div>
                     <h2>What are they facing?</h2>
-                    <h4>Delivery Problem. </h4>
-                    <p>“Sometimes when they knock on the door, and by the time I ... open the door, they’ve left the food hanging (on my gate),” he said. “The first time, I didn't realise the food was there.”</p>
-                    <a href="https://www.channelnewsasia.com/cnainsider/time-covid-pandemic-challenges-faced-visually-impaired-singapore-937276">Find Out More</a>
-                    <br></br>
-                    <br></br>
-                    <h4>Navigating A New Landscape</h4>
-                    <p>“It was kind of irritating because we spent more time looking for seats”</p>
-                    <p>“So my walking memory is no longer of use because I can’t go by certain routes any more”</p>
-                    <a href="https://www.channelnewsasia.com/cnainsider/time-covid-pandemic-challenges-faced-visually-impaired-singapore-937276">Find Out More</a>
+                    <h4>Problems in navigating around </h4>
+                    <p>Environmental Challenges
+Without any aid from technology or human assistance, the visually impaired must memorize the location of every obstacle or item in their home environment and not being able to see means that they have a difficult time navigating outside the spaces that they're accustomed to and are prone to accidents happening.</p>
                 </div>
             </div>
             
--- src/components/Guide/Guide.js
+++ src/components/Guide/Guide.js
@@ -12,7 +12,7 @@ const Home = () => {
                 <h3>Step 3</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
-            <iframe  className="youtube" width="560" height="315" src="https://www.youtube-nocookie.com/embed/0yZcDeVsj_Y" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"></iframe>
+            <iframe  className="youtube" src="https://www.youtube-nocookie.com/embed/0yZcDeVsj_Y" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"></iframe>
         </div>
     </div>
 }   
