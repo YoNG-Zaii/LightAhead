3e15c006e68c9557be0949c8eb2f3025aa1a953f
YoNG-Zaii <yongzaii811@outlook.com>
2022-10-02 12:21:06 +0800

    Adjusted Homepage

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -7,6 +7,14 @@ const HomePage = () => {
                 <div className='Text'>
                     <h1>LightAhead™</h1>
                     <p>A Vision for the Blind</p>
+                    <div className='Team'>
+                        <h3>Avengers Assemble</h3>
+                        <button><a href='https://github.com/blws02'>Bernard Lim</a></button>
+                        <button><a href='https://github.com/Horstann'>Horstann Ho</a></button>
+                        <button><a href='https://github.com/YoNG-Zaii/'>Guo Yong</a></button>
+                        <button><a href='https://github.com/Pratham117'>Pratham</a></button>
+                        <button><a href='https://github.com/SLAU925'>Shao Wei</a></button>
+                    </div>
                 </div>
             </div>
             <img id='bgImg' 
